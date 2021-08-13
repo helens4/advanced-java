052c09f0f1a0e5147976bc9df2a8846f45005928
helens4 <49526171+helens4@users.noreply.github.com>
2021-08-14 00:16:32 +0200

    changed piatek trzynastego

--- src/app/App.java
+++ src/app/App.java
@@ -2,6 +2,8 @@ package app;
 
 class App {
     public static void main(String[] args) {
-     System.out.println("piatek trzynastego");
+     System.out.println("piatek trzynastegooo");
     }
 }
+
+//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
