6c8dd2db108295b6c2b3687361ac8fa782a0fa07
helens4 <49526171+helens4@users.noreply.github.com>
2021-08-14 10:50:18 +0200

    dodalem aaaa

--- src/app/App.java
+++ src/app/App.java
@@ -4,7 +4,7 @@ class App {
     public static void main(String[] args) {
      System.out.println("piatek czynastegooo");
      System.out.println("sóbota czternastego");
-     //bikini
+     //aaaa
     }
 }
 
